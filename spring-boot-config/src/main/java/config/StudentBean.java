dac008540d1a66ca531b5a18a36bc4854bdd4ad2
wei jin <weijin1@lenovo.com>
2019-05-07 17:56:32 +0800

    add some feature

---
+++ spring-boot-config/src/main/java/config/StudentBean.java
@@ -0,0 +1,30 @@
+package config;
+
+/**
+ * Company lenovo.com Copyright (C) 1984-2019 All Rights Reserved.
+ *
+ * @author david
+ * @version StudentBean.java, v 0.1 2019-05-07 16:20 david
+ * @project spring-boot-learning-examples
+ */
+public class StudentBean {
+
+  private int id;
+  private String name;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
